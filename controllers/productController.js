1080ecb0dbaa96c853b31b9c560c37ca24fe9230
Manish-ascon <manish@asconsoftech.com>
2025-11-23 00:43:00 +0530

    error solve

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,3 +1,4 @@
+
 const Product = require("../models/Product");
 
 exports.createProduct = async (req, res) => {
@@ -24,4 +25,15 @@ exports.createProduct = async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
+  
 };
+
+exports.getProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router = express.Router();
 const upload = require("../middleware/upload");
-const { createProduct } = require("../controllers/productController");
+
+// Import all controller functions
+const { createProduct, getProducts } = require("../controllers/productController");
 
 // Upload up to 4 images
 router.post("/create", upload.array("images", 4), createProduct);
-router.get("/all", productController.getProducts);
+
+// Fetch all products
+router.get("/all", getProducts);
 
 module.exports = router;
